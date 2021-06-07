e0ccc139398679c4cd6b2b57229ec7a745b1b053
Andrew Keiser <webdev410@gmail.com>
2021-06-06 23:08:37 -0500

    got it working but it's still looping through twice before displaying the password

--- script.js
+++ script.js
@@ -14,156 +14,92 @@ generateBtn.addEventListener("click", writePassword);
 
 // MY CODE
 
-// Prompts
+// FUNCTION START !!!!!!!!!!!!************
 
 function generatePassword() {
 
-  
-  var upperCaseArray = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ];
-  var lowerCaseArray = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-  var specialCharacterArray = ["!", "@", "#", "$", "%", "&", "*"];
-  var numericCharacterArray = [
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-  ];
+    // Defining all possibilities
+      var upperCaseArray =  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+      var lowerCaseArray =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+      var specialCharacterArray = ["!", "@", "#", "$", "%", "&", "*"];
+      var numericCharacterArray = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-  var allCharactersArray = [];
-  var randomPassword = "";
+    // will combine elements depending on user input
 
-  var passwordLength = prompt(
-    "How many characters? Must be between 8 and 128 Characters."
-  );
+      var allCharactersArray = [];
 
+    // the final result variable
+      var password = "";
 
+    // start of prompts
+      var passwordLength = prompt(
+        "How many characters? Must be between 8 and 128 Characters."
+      );
 
-  if (!passwordLength) {
-    return;
-  }
+      // make sure the length is between 8 and 128 characters
 
-  while (passwordLength < 8 || passwordLength > 128) {
-    window.alert("Please enter a number between 8 and 128.");
-    var passwordLength = prompt(
-      "How many characters?",
-      "Must be between 8 and 128 Characters."
-    );
-  }
+      while (passwordLength < 8 || passwordLength > 128) {
+        window.alert("Please enter a number between 8 and 128.");
+        var passwordLength = prompt(
+          "How many characters? Must be between 8 and 128 Characters."
+        );
+      }
+    // defining password requirements
+        var lowerCase = confirm("Do you want to include lower case characters?");
+        var upperCase = confirm("Do you want to include upper case characters?");
+        var numeric = confirm("Do you want to include numeric characters?");
+        var special = confirm("Do you want to include special characters?");
 
- 
+    // Combine allCharactersArray per confirm responses True = they should be added to the array.
+      if (lowerCase === true) {
+        var allCharactersArray = allCharactersArray.concat(lowerCaseArray);
+        console.log(allCharactersArray);
+      }
 
-  var upperCase = confirm("Do you want to include uppercase characters?");
-  var lowerCase = confirm("Do you want to include lower case characters?");
-  var numeric = confirm("Do you want to include numeric characters?");
-  var special = confirm("Do you want to include special characters?");
+      if (upperCase === true) {
+        var allCharactersArray = allCharactersArray.concat(upperCaseArray);
+        console.log(allCharactersArray);
+      }
 
-  // Conditions for the password. If they should be added to the array.
+      if (numeric === true) {
+        var allCharactersArray = allCharactersArray.concat(numericCharacterArray);
+        console.log(allCharactersArray);
+      }
 
-  if (lowerCase === true) {
-    var allCharactersArray = allCharactersArray.concat(lowerCaseArray);
+      if (special === true) {
+        var allCharactersArray = allCharactersArray.concat(specialCharacterArray);
+        console.log(allCharactersArray);
+      }
 
-    console.log("ALL CHARACTERS", allCharactersArray);
-  }
+   // for loop to generate random password
+      for (var i = 0; i<passwordLength; i++) {
+        var randomCharacter = allCharactersArray[Math.floor(Math.random()*allCharactersArray.length)];
+        
+        password = password + randomCharacter
 
-  if (upperCase === true) {
-    var allCharactersArray = allCharactersArray.concat(upperCaseArray);
+      }
 
-    console.log("ALL CHARACTERS", allCharactersArray);
-  }
+      console.log(password)
+      return password;
 
-  if (numeric === true) {
-    var allCharactersArray = allCharactersArray.concat(numericCharacterArray);
+      
+      // if (passwordLength === password.length) {
+      //   return password;
+      // }
 
-    console.log("ALL CHARACTERS", allCharactersArray);
-  }
+      // make sure at least one variable is chosen
+      if (
+        special === false &&
+        numeric === false &&
+        upperCase === false &&
+        lowerCase === false
+      ) {
+        alert("Error: You must select at least one character type");
+      }
 
-  if (special === true) {
-    var allCharactersArray = allCharactersArray.concat(specialCharacterArray);
-
-    console.log("ALL CHARACTERS", allCharactersArray);
-  }
-
-  if (
-    special === false &&
-    numeric === false &&
-    upperCase === false &&
-    lowerCase === false
-  ) {
-    alert("Error: You must select at least one character type");
-  }
+      // Write password to text area
 
 
-  // for loop to generate random password
-for (var i=0; i < passwordLength; i++){
-  var randomCharacter = allCharactersArray[Math.floor(Math.random() * allCharactersArray.length)]
- 
-  // password += randomCharacter
-  randomPassword = randomPassword + randomCharacter
-  
-}
- if (passwordLength === randomPassword.length) {
-  return randomPassword;
- }
 
 }
 
